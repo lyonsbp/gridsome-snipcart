93f72387f429bd26f7a6e34b00f244c212753032
Brandon Lyons <lyonsbp@umich.edu>
2019-06-28 21:46:11 -0400

    Add store Remove package-lock

---
+++ src/pages/Store.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="products">
+    <product
+      class="product"
+      v-for="product in products"
+      :key="product.title"
+      :title="product.title"
+      :img="product.img"
+    />
+  </div>
+</template>
+
+<script>
+import Product from '@/components/Product'
+export default {
+  components: {
+    Product
+  },
+  data: () => ({
+    products: [
+      {
+        title: 'Test Title',
+        img: 'https://via.placeholder.com/150'
+      },
+      {
+        title: 'Test Title',
+        img: 'https://via.placeholder.com/150'
+      },
+      {
+        title: 'Test Title',
+        img: 'https://via.placeholder.com/150'
+      }
+    ]
+  })
+}
+</script>
+
+<style>
+.products {
+  display: flex;
+}
+.product {
+  margin-left: 1em;
+  margin-right: 1em;
+}
+</style>
