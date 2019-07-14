ecef1082b4bec7b02dbae8a8ecf22fdcb5ef4f03
Brandon Lyons <lyonsbp@umich.edu>
2019-07-14 15:57:46 -0400

    Include add to cart button

--- src/pages/Store.vue
+++ src/pages/Store.vue
@@ -1,20 +1,19 @@
 <template>
   <Layout>
     <div class="products">
-      <div
-        v-for="edge in $page.products.edges"
-        class="product--container"
-        :key="edge.node.id"
-        :data-item-id="edge.node.id"
-        :data-item-url="edge.node.path"
-        :data-item-name="edge.node.title"
-        :data-item-image="edge.node.image"
-        data-item-price="10.00"
-      >
+      <div v-for="edge in $page.products.edges" class="product--container" :key="edge.node.id">
         <h2 class="product--title" v-text="edge.node.title"></h2>
         <g-link :to="edge.node.path">
           <img :src="edge.node.image" height="200px" />
         </g-link>
+        <button
+          class="snipcart-add-item"
+          :data-item-id="edge.node.id"
+          :data-item-url="edge.node.path"
+          :data-item-name="edge.node.title"
+          :data-item-image="edge.node.image"
+          data-item-price="10.00"
+        >Add to Cart</button>
       </div>
     </div>
   </Layout>
