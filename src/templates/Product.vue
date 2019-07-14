b196eeb450ccc7d3c1e6c9d912bcb6ec2eb9f18b
Brandon Lyons <lyonsbp@umich.edu>
2019-07-14 16:28:25 -0400

    Add cart button to individual porduct pages

--- src/templates/Product.vue
+++ src/templates/Product.vue
@@ -4,6 +4,14 @@
       <h2 class="product--title" v-text="$page.product.title"></h2>
       <img :src="$page.product.image" width="250px" />
       <span v-html="$page.product.content"></span>
+      <button
+        class="snipcart-add-item"
+        :data-item-id="$page.product.id"
+        :data-item-url="$page.product.path"
+        :data-item-name="$page.product.title"
+        :data-item-image="$page.product.image"
+        data-item-price="10.00"
+      >Add to Cart</button>
     </div>
   </Layout>
 </template>
@@ -11,6 +19,8 @@
 <page-query>
 query Product($id: String!) {
   product: product(id: $id) {
+    id
+    path
     title
     image
     content
